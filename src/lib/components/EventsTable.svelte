f3dc9e1633ad5f6553d786d5116813c96bfb1c35
tree <tree@hidden.cz>
2023-04-02 00:06:23 +0200

    Update

--- src/lib/components/EventsTable.svelte
+++ src/lib/components/EventsTable.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { isFuture } from 'date-fns';
+
     export let events;
     export let chronicle;
 
@@ -9,9 +11,9 @@
 
 <div class="table">
     {#each events.sort((x, y) => x.date < y.date ? 1 : -1) as event}
-        <div class="table-row">
+        <div class="table-row {isFuture(new Date(event.date)) ? 'text-gray-300' : '' }">
             <div class="table-cell font-mono text-sm pr-2">{event.date}</div>
-            <div class="table-cell pr-2"><a href="/e/{event.id}">{event.name}</a></div>
+            <div class="table-cell pr-2"><a href="/{event.id}">{event.name}</a></div>
             <div class="table-cell">{getSeries(event.series).place || '-'}</div>
         </div>
     {/each}
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -6,10 +6,13 @@
 
     $: item = data.chronicle.events.find(e => e.id === $page.params.id)
     $: series = item.series ? data.chronicle.series.find(s => s.id === item.series) : null
+    $: ghLink = `https://github.com/ethevents-club/chronicle/blob/main/data/events/${item.date.match(/^(\d{4})/)[1]}/${item.id}/index.yaml`
 </script>
 
 <div class="">
     <h2 class="font-bold">{item.name}</h2>
-    <div>Place: {series?.place}</div>
+    <div>Series: {#if series}<a href="/s/{series.id}">{series.name}</a> ({series.place}){:else}-{/if}</div>
     <div class="my-2 p-2 text-sm bg-gray-100"><pre><code>{JSON.stringify(item, null, 2)}</code></pre></div>
+    <hr />
+    <div>Edit this page on GitHub: <a href={ghLink}>{ghLink}</a></div>
 </div>
